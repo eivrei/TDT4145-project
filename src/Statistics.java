d1181e5646aa11858c6f81fbac42905bacb5931b
eivrei <22083159+eivrei@users.noreply.github.com>
2017-03-14 20:58:44 +0100

    Fix SQL statements in Exercise, Result and Statistics. Add user stories to README

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,66 +1,58 @@
-
-import sun.java2d.pipe.SpanShapeRenderer;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
 
 
 public class Statistics extends Connector{
-
     public void getTopResultStrenght(int ovelseId, String startDato, String sluttDato) {
-
         try {
             Statement statement=conn.createStatement();
-            ResultSet rs=statement.executeQuery("select dato, belastning,antallRep,antallSett from Resultat where ovelseId='"+ovelseId+"' and dato BETWEEN '"+startDato+"' and '"+sluttDato+"' and belastning IS NOT NULL order by belastning ;");
+            ResultSet rs=statement.executeQuery("SELECT dato, belastning, antallRep, antallSett FROM Resultat " +
+                                                     "WHERE ovelseId='"+ovelseId+"' AND dato BETWEEN '"+startDato+
+                                                     "' AND '"+sluttDato+"' AND belastning IS NOT NULL " +
+                                                     "ORDER BY belastning ;");
             while (rs.next()){
-                System.out.println("{" + rs.getDate("dato") + "|" + rs.getInt("belastning") + "|" + rs.getInt("antallRep") + "|" + rs.getInt("antallSett") + "}");
+                System.out.println("{" + rs.getDate("dato") + "|" + rs.getInt("belastning") +
+                        "|" + rs.getInt("antallRep") + "|" + rs.getInt("antallSett") + "}");
             }
-
         }
-
         catch (SQLException e) {
             System.out.println(e);
         }
     }
 
-    public void getTopResultrunning(int ovelseId, String startDato, String sluttDato) {
-
+    public void getTopResultRunning(int ovelseId, String startDato, String sluttDato) {
         try {
             Statement statement=conn.createStatement();
-            ResultSet rs=statement.executeQuery("select dato,varighet,distanse from Resultat where ovelseId='"+ovelseId+"' and dato BETWEEN '"+startDato+"' and'"+sluttDato+"' and distanse IS NOT NULL order by distanse ;");
+            ResultSet rs=statement.executeQuery("SELECT dato, varighet, distanse FROM Resultat " +
+                                                     "WHERE ovelseId='"+ovelseId+"' AND dato BETWEEN '"+startDato+"' " +
+                                                     "AND'"+sluttDato+"' AND distanse IS NOT NULL " +
+                                                     "ORDER BY distanse ;");
             while (rs.next()) {
-                System.out.println("{" + rs.getDate("dato") + "|" + rs.getInt("varighet") + "|" + rs.getInt("distanse") + "}");
+                System.out.println("{" + rs.getDate("dato") + "|" + rs.getInt("varighet") + "|"
+                                   + rs.getInt("distanse") + "}");
             }
         }
-
-
-
-
         catch (SQLException e) {
-            System.out.println(e);
+            System.out.println("Error getting topResultsRunning: " + e);
         }
     }
-
 
     public void getStatistics(String startDato, String sluttDato) {
-
         try {
             Statement statement=conn.createStatement();
-            ResultSet rs=statement.executeQuery("select SUM(varighet) from Treningsokt where dato BETWEEN '"+startDato+"' and '"+sluttDato+"' ;");
+            ResultSet rs=statement.executeQuery("SELECT SUM(varighet) FROM Treningsokt " +
+                                                     "WHERE dato BETWEEN '"+startDato+"' AND '"+sluttDato+"' ;");
             int sumVarighet=0;
             while(rs.next()) {
                 int i=rs.getInt(1);
                 sumVarighet+= i;
             }
-
-            System.out.println("I Perioden " + startDato + " til " + sluttDato + " har du trent i " + sumVarighet +" minutter.");
-
+            System.out.println("I Perioden " + startDato + " til " + sluttDato + " har du trent i "
+                               + sumVarighet +" minutter.");
         }
-
         catch (SQLException e) {
-            System.out.println(e);
+            System.out.println("Error getting statistics " + e);
         }
     }
 
@@ -68,8 +60,7 @@ public class Statistics extends Connector{
         Statistics test = new Statistics();
         test.connect();
         test.getStatistics("2017.03.01","2017.04.01");
-        test.getTopResultrunning(5, "2017.03.01", "2017.04.01");
+        test.getTopResultRunning(5, "2017.03.01", "2017.04.01");
         test.getTopResultStrenght(2, "2017.03.01", "2017.04.01");
     }
-
 }
